e7f81f6b22e654c778cf80e7b5f1ff27f0d790b0
Your Name <you@example.com>
2017-05-05 14:19:51 +0200

    updates

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ git clone https://github.com/Revenberg/ansible-tower.git
 
 # Configure IP address in "hosts" file. If you have more than one
 # Raspberry Pi, add more lines and enter details
-i=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+i=$(sudo ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 h=$(hostname)
 
 echo "$h ansible_host=$i" >> /home/pi/ansible.log
