5c027629517962214468128d663e51ea217339b2
Sander Revenberg <sander@revenberg.info>
2017-05-05 15:49:37 +0200

    updates

--- install-tower.sh
+++ install-tower.sh
@@ -8,16 +8,19 @@ git clone https://github.com/Revenberg/ansible.git
 git clone https://github.com/Revenberg/ansible-tower.git 
 
 # Configure IP address in "hosts" file. If you have more than one
-# Raspberry Pi, add more lines and enter details
 i=$(sudo ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 h=$(hostname)
 
-echo "$h ansible_host=$i" >> /home/pi/ansible.log
+echo "$h ansible_host=$i" >> ~/ansible.log
 
-echo "[tower]" >> /home/pi/ansible/hosts
+echo "[tower]" >> ~/ansible/hosts
 echo "$i  ansible_connection=ssh ansible_ssh_user=pi ansible_ssh_pass=tower" >> ~/ansible/hosts
 
 cd ~/ansible-tower
 ansible-playbook setup.yml >> ~/ansible.log
 
 cat ~/ansible.log
+
+echo "logon tower:tower
+echo "wget wget https://raw.githubusercontent.com/Revenberg/ansible-tower/master/install-tower.sh && chmod +x install-tower.sh"
+
